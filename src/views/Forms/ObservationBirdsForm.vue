7bd1cdc62273038bc20f154c87236e70b6df22b1
Jimmy <jimmylefrancois38@gmail.com>
2023-12-23 23:42:01 +0100

    v-dialog before deleting bird

--- src/views/Forms/ObservationBirdsForm.vue
+++ src/views/Forms/ObservationBirdsForm.vue
@@ -1,6 +1,32 @@
 <template>
   <h3 class="py-2 text-center">{{ currentObservationListItem.location }}</h3>
   <EndObservation @endObservation="endObservation"/>
+  <v-dialog
+    v-model="displayBirdRemoveDialog"
+    width="auto"
+  >
+    <v-card title="Retirer une espèce">
+      <v-card-text>
+        Souhaitez-vous retirer cette espèce de vos observation ?
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="red"
+          prepend-icon="mdi-cancel"
+          @click="displayBirdRemoveDialog = false"
+        >Annuler
+        </v-btn>
+        <v-btn
+          color="green"
+          prepend-icon="mdi-check"
+          @click="removeBirdFormList"
+        >Supprimer
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
   <v-autocomplete
     variant="solo-filled"
     :items="birdsList"
@@ -17,7 +43,7 @@
     no-data-text="Aucun oiseau observé."
   >
     <template v-slot:item="{ item }">
-      <BirdItemRow :bird="item" @removeBird="removeBirdFormList($event)"/>
+      <BirdItemRow :bird="item" @removeBird="tryToRemoveBirdFromList($event)"/>
     </template>
     <template v-slot:bottom>
     </template>
@@ -30,15 +56,24 @@ import {ref, watch} from "vue";
 import BirdItemRow from "@/components/BirdItemRow";
 import EndObservation from "@/components/EndObservation";
 import {format} from "date-fns";
-import { useObservationsStore } from "@/store/observations";
+import {useObservationsStore} from "@/store/observations";
 import {storeToRefs} from "pinia";
 
 const observationStore = useObservationsStore()
-const { clearCurrentObservationItem } = observationStore
-const { currentObservationListItem } = storeToRefs(observationStore)
+const {clearCurrentObservationItem} = observationStore
+const {currentObservationListItem} = storeToRefs(observationStore)
+const birdToRemoveIndex = ref(null)
+const displayBirdRemoveDialog = ref(false)
 
-function removeBirdFormList(index) {
-  currentObservationListItem.value.observedBirds.splice(index, 1)
+function removeBirdFormList() {
+  currentObservationListItem.value.observedBirds.splice(birdToRemoveIndex.value, 1)
+  birdToRemoveIndex.value = null
+  displayBirdRemoveDialog.value = false
+}
+
+function tryToRemoveBirdFromList(index) {
+  birdToRemoveIndex.value = index
+  displayBirdRemoveDialog.value = true
 }
 
 function endObservation() {
