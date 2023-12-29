c631bbc34cbb9b2957b8033eb48874efbc282e15
Jimmy <jimmylefrancois38@gmail.com>
2023-12-29 14:09:04 +0100

    Routes one by one to add separator

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <!--  <v-app :style="{background: $vuetify.theme.themes['light'].colors.themeWhiteColor}">-->
-  <v-app style="background-image: url('./background.jpg');">
+  <v-app style="background-image: url('/background.jpg');">
     <v-container :fluid="true">
       <v-app-bar
         color="themeLightgreenColor"
@@ -8,6 +8,7 @@
       >
         <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
         <v-app-bar-title>{{ router.currentRoute.value.name }}</v-app-bar-title>
+        <v-btn v-if="loggedUser && !loggedUser.isAnonymous" @click="logout" density="compact" icon="mdi-logout"></v-btn>
       </v-app-bar>
 
       <v-navigation-drawer
@@ -18,11 +19,21 @@
           :nav="true"
           density="compact"
         >
-          <v-list-item title="Mes observations ornitho"></v-list-item>
-          <v-divider></v-divider>
-          <v-list-item v-for="(route, index) in router.getRoutes()" :key="index" :link="true" :title="route.name" :to="route.path"></v-list-item>
+          <v-list-subheader>Mes observation ornitho</v-list-subheader>
+          <v-list-item :link="true" title="Accueil" :to="{'name': 'Accueil'}" prepend-icon="mdi-home"></v-list-item>
+          <v-list-item :link="true" title="Nouvelle observation" :to="{'name': 'Nouvelle observation'}"  prepend-icon="mdi-plus"></v-list-item>
+          <v-list-item :link="true" title="Mes observations" :to="{'name': 'Mes observations'}"  prepend-icon="mdi-format-list-bulleted"></v-list-item>
+          <v-list-subheader>Mon compte</v-list-subheader>
+          <template v-if="!loggedUser" >
+            <v-list-item :link="true" title="Me connecter" :to="{'name': 'Connexion'}" prepend-icon="mdi-account"></v-list-item>
+            <v-list-item :link="true" title="Créer mon compte" :to="{'name': 'Créer mon compte'}" prepend-icon="mdi-account-plus"></v-list-item>
+          </template>
+          <template v-else>
+            <v-list-item :link="true" title="Me déconnecter" @click="logout" prepend-icon="mdi-logout"></v-list-item>
+          </template>
         </v-list>
       </v-navigation-drawer>
+      <v-spacer />
       <v-row justify="center" align="center" class="mt-10">
         <v-col cols="12">
           <router-view/>
@@ -35,6 +46,22 @@
 <script setup>
   import {ref} from "vue";
   import router from "@/router"
-
+  import {useUsersStore} from "@/store/users";
+  import {storeToRefs} from "pinia";
+  import { auth } from './conf/firebase'
+  import { signOut } from 'firebase/auth'
+  const userStore = useUsersStore()
+  const { loggedUser } = storeToRefs(userStore)
   const drawer = ref(false)
+
+  function logout() {
+    signOut(auth).then(() => {
+      //todo snackbar
+      // Sign-out successful.
+    }).catch((error) => {
+      //todo snackbar
+      // An error happened.
+    });
+  }
+
 </script>
