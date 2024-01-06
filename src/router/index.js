3dccc3cf86e8d878ce77728986bd94e914d94b7a
Jimmy <jimmylefrancois38@gmail.com>
2024-01-06 13:54:31 +0100

    createWebHashHistory, github page ne fait pas de rewrite

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import { getCurrentUser } from '@/conf/firebase'
 import {useHead} from "@unhead/vue";
 
@@ -47,7 +47,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
