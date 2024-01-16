5ae58e1dab1fe2d8594309687b66f5a07f62331c
Jimmy <jimmylefrancois38@gmail.com>
2024-01-16 22:27:57 +0100

    test pwa

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,37 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: "Plumes en vue",
+        short_name: 'Plumesenvue',
+        description: 'Recensez toutes vos observations ornithologiques au même endroit !',
+        theme_color: '#9DA65D',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
       }
     }),
     ViteFonts({
