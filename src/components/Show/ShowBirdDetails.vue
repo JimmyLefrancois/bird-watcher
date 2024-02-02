17ffd6bd89405cc333cd8d206b02811d26222092
Jimmy <jimmylefrancois38@gmail.com>
2024-02-03 00:05:24 +0100

    Composant détails de l'oiseau

---
+++ src/components/Show/ShowBirdDetails.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-list-item class="py-0">
+    <v-list-item-subtitle>
+      ⓘ Observé à {{ getHoursAndMinutes(bird.date) }} | Sexe : {{ getGender() }}
+    </v-list-item-subtitle>
+  </v-list-item>
+</template>
+
+<script setup>
+import {format} from "date-fns";
+
+const props = defineProps({
+  bird: {type: Object, default: null}
+})
+
+const getGender = () => {
+  if (!props.bird.gender) {
+    return 'N/C'
+  }
+  return props.bird.gender === 'male' ? 'Mâle' : 'Femelle'
+}
+
+const getHoursAndMinutes = (date) => {
+  return format(new Date(date), 'HH:mm')
+}
+</script>
+
+<style scoped>
+
+</style>
