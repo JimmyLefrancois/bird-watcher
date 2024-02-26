241644347b219902b7c1968ff012b49199582f70
Jimmy <jimmylefrancois38@gmail.com>
2024-02-26 23:12:12 +0100

    feat(geolocation): Ajout de la geolocalisation dans BirdItemRow

--- src/components/BirdItemRow.vue
+++ src/components/BirdItemRow.vue
@@ -16,17 +16,27 @@ import { findBirdInBirdsList } from "@/helpers/birdHelpers";
 import { useObservationsStore } from "@/store/observations";
 import {storeToRefs} from "pinia";
 import {format} from "date-fns";
+import {useGeolocationStore} from "@/store/geolocation";
 
 const observationStore = useObservationsStore()
 const { currentObservationListItem, currentObservationToHandle } = storeToRefs(observationStore)
 const { updateBirdsListFromCurrentObservation } = observationStore
 
+const geolocationStore = useGeolocationStore()
+const {geolocationPermissionStore} = storeToRefs(geolocationStore)
+
 const props = defineProps({
-  bird: {type: Object, default: null}
+  bird: {type: Object, default: null},
+  coordinates: {type: Object, default: null}
 })
 
 function addBird() {
-  currentObservationToHandle.value.observedBirds.push({id: props.bird.id, date: format(new Date(), "yyyy-MM-dd'T'HH:mm"), customId: crypto.randomUUID()})
+  const birdToAdd = {id: props.bird.id, date: format(new Date(), "yyyy-MM-dd'T'HH:mm"), customId: crypto.randomUUID()}
+  if (geolocationPermissionStore.value === 'granted') {
+    birdToAdd.longitude = props.coordinates.longitude
+    birdToAdd.latitude = props.coordinates.latitude
+  }
+  currentObservationToHandle.value.observedBirds.push(birdToAdd)
   if (!currentObservationToHandle.value.endDate) {
     updateBirdsListFromCurrentObservation(currentObservationListItem.value)
   }
