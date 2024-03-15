df646b3b7954a70f234494213738c2f276a15af8
Jimmy <jimmylefrancois38@gmail.com>
2024-03-15 20:26:29 +0100

    fix: Fix coordinates missing .value

--- src/components/BirdItemRow.vue
+++ src/components/BirdItemRow.vue
@@ -33,8 +33,8 @@ const props = defineProps({
 function addBird() {
   const birdToAdd = {id: props.bird.id, date: format(new Date(), "yyyy-MM-dd'T'HH:mm"), customId: crypto.randomUUID()}
   if (isUsingGeolocation.value && props.type === 'create') {
-    birdToAdd.longitude = coordinates.longitude
-    birdToAdd.latitude = coordinates.latitude
+    birdToAdd.longitude = coordinates.value.longitude
+    birdToAdd.latitude = coordinates.value.latitude
   }
   currentObservationToHandle.value.observedBirds.push(birdToAdd)
   if (!currentObservationToHandle.value.endDate) {
